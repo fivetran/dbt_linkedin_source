2c4754b6269dd948ff21c8e4269db550484902e8
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-11-20 17:03:35 -0600

    update macro

--- models/stg_linkedin_ads__creative_history.sql
+++ models/stg_linkedin_ads__creative_history.sql
@@ -43,11 +43,11 @@ with base as (
         {{ dbt.split_part('click_uri', "'?'", 1) }} as base_url,
         {{ dbt_utils.get_url_host('click_uri') }} as url_host,
         '/' || {{ dbt_utils.get_url_path('click_uri') }} as url_path,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_source') }} as utm_source,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_medium') }} as utm_medium,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_campaign') }} as utm_campaign,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_content') }} as utm_content,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_term') }} as utm_term
+        {{ extract_uri_parameter('click_uri', 'utm_source') }} as utm_source,
+        {{ extract_uri_parameter('click_uri', 'utm_medium') }} as utm_medium,
+        {{ extract_uri_parameter('click_uri', 'utm_campaign') }} as utm_campaign,
+        {{ extract_uri_parameter('click_uri', 'utm_content') }} as utm_content,
+        {{ extract_uri_parameter('click_uri', 'utm_term') }} as utm_term
     
     from fields
 )
