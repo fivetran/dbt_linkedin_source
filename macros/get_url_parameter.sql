79ffd82180f3e13254074ab5755996fbbba65ce6
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-11-20 15:12:56 -0600

    bug/databricks-get-url-parameter

---
+++ macros/get_url_parameter.sql
@@ -0,0 +1,25 @@
+{% macro get_url_parameter(field, url_parameter) -%}
+    {{ return(adapter.dispatch('get_url_parameter', 'dbt_utils')(field, url_parameter)) }}
+{% endmacro %}
+
+{% macro default__get_url_parameter(field, url_parameter) -%}
+
+{%- set formatted_url_parameter = "'" + url_parameter + "='" -%}
+
+{%- set split = dbt.split_part(dbt.split_part(field, formatted_url_parameter, 2), "'&'", 1) -%}
+
+nullif({{ split }},'')
+
+{%- endmacro %}
+
+
+{% macro databricks__get_url_parameter(field, url_parameter) -%}
+
+{%- set formatted_url_parameter = "'" + url_parameter + "='" -%}
+
+case 
+    when regexp_extract({{ field }}, concat({{ formatted_url_parameter }}, '([^&]+)'), 1) != ''
+    then regexp_extract({{ field }}, concat({{ formatted_url_parameter }}, '([^&]+)'), 1)
+    else null end #}
+
+{%- endmacro %}
