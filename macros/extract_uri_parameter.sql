42544c7bcb011d38b0fd4934c4358fcf03704fcd
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-11-22 16:08:18 -0600

    update macro

--- macros/extract_uri_parameter.sql
+++ macros/extract_uri_parameter.sql
@@ -1,12 +1,20 @@
 {% macro extract_uri_parameter(field, uri_parameter) -%}
-    {{ return(adapter.dispatch('extract_uri_parameter', 'linkedin_source')(field, uri_parameter)) }}
+
+{{ adapter.dispatch('extract_uri_parameter', 'linkedin_source') (field, uri_parameter) }}
+
 {% endmacro %}
 
+
 {% macro default__extract_uri_parameter(field, uri_parameter) -%}
+
 {{ dbt_utils.get_url_parameter(field, uri_parameter) }}
+
 {%- endmacro %}
 
+
 {% macro databricks__extract_uri_parameter(field, uri_parameter) -%}
-{%- set formatted_uri_parameter = "'" + uri_parameter + "='" -%}
-nullif(regexp_extract({{ field }}, concat({{ formatted_uri_parameter }}, '([^&]+)'), 1),'')
+
+{%- set formatted_uri_parameter = "'" + uri_parameter + "=([^&]+)'" -%}
+nullif(regexp_extract({{ field }}, {{ formatted_uri_parameter }}, 1), '')
+
 {%- endmacro %}
