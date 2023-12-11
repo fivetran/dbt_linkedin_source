0c6903af841f7a7338be1bf4b452164e271fa8b4
Joe Markiewicz <joseph.markiewicz@fivetran.com>
2023-12-11 11:04:15 -0600

    Update package to use the package extract_url_parameter macro

--- models/stg_linkedin_ads__creative_history.sql
+++ models/stg_linkedin_ads__creative_history.sql
@@ -43,11 +43,11 @@ with base as (
         {{ dbt.split_part('click_uri', "'?'", 1) }} as base_url,
         {{ dbt_utils.get_url_host('click_uri') }} as url_host,
         '/' || {{ dbt_utils.get_url_path('click_uri') }} as url_path,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_source') }} as utm_source,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_medium') }} as utm_medium,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_campaign') }} as utm_campaign,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_content') }} as utm_content,
-        {{ dbt_utils.get_url_parameter('click_uri', 'utm_term') }} as utm_term
+        {{ linkedin_source.linkedin_ads_extract_url_parameter('click_uri', 'utm_source') }} as utm_source,
+        {{ linkedin_source.linkedin_ads_extract_url_parameter('click_uri', 'utm_medium') }} as utm_medium,
+        {{ linkedin_source.linkedin_ads_extract_url_parameter('click_uri', 'utm_campaign') }} as utm_campaign,
+        {{ linkedin_source.linkedin_ads_extract_url_parameter('click_uri', 'utm_content') }} as utm_content,
+        {{ linkedin_source.linkedin_ads_extract_url_parameter('click_uri', 'utm_term') }} as utm_term
     
     from fields
 )
