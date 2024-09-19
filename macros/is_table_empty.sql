f87476dd41183d34cdc2f6e6e18a84762af6b808
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2024-09-19 09:55:49 -0500

    review updates

--- macros/is_table_empty.sql
+++ macros/is_table_empty.sql
@@ -7,14 +7,11 @@
 {%- macro default__is_table_empty(table_name) -%}
     {%- if execute and flags.WHICH in ('run', 'build') %}
         {% set row_count_query %}
-            select count(*) as row_count from {{ table_name }}
+            select 1 from {{ table_name }} limit 1
         {% endset %}
         {% set results = run_query(row_count_query) %}
-        {% if results %}
-            {% set row_count = results.columns[0][0] %}
-            {% if row_count == 0 %}
-                {{ return("empty") }}
-            {% endif %}
+        {% if not results %}
+            {{ return("empty") }}
         {% endif %}
     {% endif -%}
 {%- endmacro -%}
